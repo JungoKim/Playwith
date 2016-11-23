6e0c27bd7835d5095bd3af82adc95790bd3e9007
EC2 Default User <ec2-user@ip-172-31-4-254.ap-northeast-1.compute.internal>
2016-11-23 14:35:56 +0000

    Introduce server initial code

---
+++ server/server.js
@@ -0,0 +1,174 @@
+// AWS configuration
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'ap-northeast-2'});
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// Data Container
+var Set = require('Set');
+
+// Express configuration
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+
+
+app.get('/scan', function (req, res) {
+  var params = {
+    TableName: 'playus',
+    ScanFilter: {
+      date: {
+        ComparisonOperator: 'GT',
+        AttributeValueList: [
+          {
+            S: '0'
+          }
+        ]
+      }
+    },
+    Limit : '10',
+    ReturnConsumedCapacity: 'NONE', // optional (NONE | TOTAL | INDEXES)
+  };
+
+  dynamodb.scan(params, function(err, data) {
+    if (err){
+      console.log(err); // an error occurred
+    }
+    else {
+      console.log(data); // successful response
+      res.json(data);
+    }
+  });
+});
+
+// createPlay
+app.put('/createPlay', function (req, res) {
+  console.log("body: " + JSON.stringify(req.body));
+
+  var currentTime = new Date().getTime().toString();
+  var state = "open";
+  var userId = req.body.userId;
+  var playusIndex = userId+"_"+currentTime;
+
+  var params = {
+    Item: {
+      "index": {
+        "S": playusIndex
+      },
+      "date": {
+        "S": currentTime
+      },
+      "state": {
+        "S": state
+      },
+      "userId": {
+        "S": userId
+      },
+      "desc": {
+        "S": req.body.desc
+      },
+      "location": {
+        "S": req.body.location
+      },
+      "locationLat": {
+        "S": req.body.locationLat
+      },
+      "locationLng": {
+        "S": req.body.locationLng
+      },
+      "playDate": {
+        "S": req.body.playDate
+      },
+      "playEvent": {
+        "S": req.body.playEvent
+      },
+      "playEventImage": {
+        "S": req.body.playEventImage
+      },
+      "joinList" : {
+        "SS": req.body.joinList
+      },
+      "maxJoin": {
+        "N": req.body.maxJoin
+      },
+      "profile": {
+        "S": req.body.profile
+      },
+    },
+    TableName: 'playus'
+  };
+
+  dynamodb.putItem(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.json(err);
+      return;
+    }
+    else {
+      console.log(JSON.stringify(data));
+      if (JSON.stringify(data) === "{}")
+        res.json('{"result" : "New play created"}');
+    }
+  });
+
+  // Find #tags and put tags to yesnoTag table
+  if (req.body.desc.indexOf('#') !== -1) {
+    console.log("desc has tag");
+
+    var words = req.body.mainContent.split(/\s+/);
+    var tagSet = new Set();
+
+    for (var i in words) {
+      if (words[i][0] === '#') {
+        tagSet.add(words[i]);
+      }
+    }
+
+    var tags = tagSet.toArray();
+
+    for (var i in tags) {
+      console.log(tags[i]);
+      var params = {
+        Item: {
+          "index": {
+            "S": playusIndex+'_'+i.toString()
+          },
+          "playusIndex": {
+            "S": playusIndex
+          },
+          "tag": {
+            "S": tags[i]
+          },
+          "date": {
+            "S": new Date().getTime().toString(),
+          },
+        },
+        TableName: 'playusTag'
+      };
+      dynamodb.putItem(params, function (err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        }
+        else {
+          console.log(JSON.stringify(data));
+        }
+      });
+    }
+  }
+});
+
+var server = app.listen(4000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Playwith app listening at http://%s:%s', host, port);
+});
+
