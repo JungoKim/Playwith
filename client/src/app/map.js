f1c059758a8b529d09312421d44200b5f8d1d49f
Jungo Kim <kismi5jung@gmail.com>
2016-11-29 19:21:06 +0900

    Display play in search map

--- client/src/app/map.js
+++ client/src/app/map.js
@@ -8,6 +8,7 @@ var {
   CircularProgress,
   RaisedButton } = mui;
 
+var searchMap;
 
 var Map = React.createClass({
   getInitialState: function() {
@@ -22,6 +23,7 @@ var Map = React.createClass({
   componentDidMount: function () {
     console.log("componentDidMount");
     this.createMap();
+    this.getPlayByLocation();
   },
 
   render: function() {
@@ -48,6 +50,7 @@ var Map = React.createClass({
         <RaisedButton
           label={window.textSet.searchInArea}
           style={styles.searchButton}
+          onTouchTap={this._handleSearchButtonTouchTap}
           primary={true} />
       </div>
     );
@@ -58,10 +61,70 @@ var Map = React.createClass({
       center: new naver.maps.LatLng(window.curLat, window.curLng),
        zoom: 7
     };
-    var map = new naver.maps.Map('map', mapOptions);
+    searchMap = new naver.maps.Map('map', mapOptions);
   },
+  _handleSearchButtonTouchTap: function() {
+    this.getPlayByLocation();
+  },
+  getPlayByLocation : function() {
+    console.log(searchMap.getBounds());
+    var lat1 = searchMap.getBounds()._min._lat;
+    var lng1 = searchMap.getBounds()._min._lng;
+    var lat2 = searchMap.getBounds()._max._lat;
+    var lng2 = searchMap.getBounds()._max._lng;
 
+    console.log(lat1);
+    console.log(lng1);
+    console.log(lat2);
+    console.log(lng2);
 
+    var query = {};
+    query.state = 'open';
+    query.locationLat1 = lat1;
+    query.locationLat2 = lat2;
+    query.locationLng1 = lng1;
+    query.locationLng2 = lng2;
+
+    $.ajax({
+      url: window.server.url+'/getPlayByLocation',
+      dataType: 'json',
+      data : query,
+      type: 'POST',
+      cache: false,
+      success: function (recievedData) {
+        console.log(recievedData.Items);
+        if (recievedData.Items !== undefined) {
+          recievedData.Items.map(function (play) {
+            if (play === null || play === undefined)
+              return;
+
+            var marker = new naver.maps.Marker({
+              position: new naver.maps.LatLng(play.locationLat.S, play.locationLng.S),
+              map: searchMap,
+              icon: {
+                content: [
+                  '<img height="42" width="42" id='+play.index.S+' src='+play.playEventImage.S+' />',
+                ].join(''),
+                size: new naver.maps.Size(42, 42),
+                anchor: new naver.maps.Point(21, 21),
+              }
+            });
+            document.getElementById(play.index.S).addEventListener('click', function(){
+              this._handlePlayInfoTouchTap(play);
+            }.bind(this));
+          }.bind(this));
+        }
+      }.bind(this),
+      error: function (xhr, status, erro) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  _handlePlayInfoTouchTap : function(play) {
+    console.log("_handlePlayInfoTouchTap");
+    selectedPlay = play;
+    this.context.router.transitionTo('play_info');
+  },
 });
 
 Map.contextTypes = {
